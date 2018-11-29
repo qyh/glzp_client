cdec27ccc509b26e2e9b26750b26fc93d85e5ccb
qyh <704344856@qq.com>
2018-11-29 11:56:28 +0800

    update config

--- test_opt.lua
+++ test_opt.lua
@@ -1,7 +1,7 @@
 return {
     {
-        startid = 700,
-        endid = 700,
+        startid = 1000,
+        endid = 1000,
         handler = "test_handler",
         param = {
             appver = "0.0.0.0", -- 版本（可选参数）
